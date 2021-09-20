90cfacb22cc56826af37ba51b8b0de0e3e729ce9
Christo <christotantony2003@gmail.com>
2021-09-20 11:20:03 +0400

    merge changes

--- course.py
+++ course.py
@@ -6,7 +6,6 @@ from slot import Slot
 from timeTable import TimeTable, MORNING_SLOTS
 
 class Course : 
-<<<<<<< HEAD
     """
     The course object that acts like a collection for similar slots.
     It allows you to have simialr titles and description among various slots, allowing for more concise code. 
@@ -34,29 +33,12 @@ class Course :
         
         assert slot > 0 and slot < (self.timeTable.SLOTS)
         if slot < self.timeTable.MORNING_SLOTS : slot -= 1
-        startTime = (self.timeTable.theorySlots[slot]) if not self.lab else self.timeTable.labSlots[slot]
-        endTime = self.timeTable.theorySlots[slot+1] if not self.lab else self.timeTable.labSlots[slot+1]
+        startTime = (self.timeTable.timeSlots[slot]) if not self.lab else self.timeTable.labSlots[slot]
+        endTime = self.timeTable.timeSlots[slot+1] if not self.lab else self.timeTable.labSlots[slot+1]
         slot = Slot(date, startTime, endTime, self.timeTable, self)
-=======
-    def __init__(self, name:str, description='', lab=False) -> None:
-        self.name = name
-        self.description = description
-        self.lab = lab
-        self.slots : List[Slot]= []
-
-    def getSlots(self) -> List[Slot]: 
-        return self.slots
-
-    def addSlot(self, timeTable: TimeTable, weekDayInt: int, slot_id: int) -> None :
-        slot_list = timeTable.labSlots if self.lab else timeTable.theorySlots
-        start_time, end_time = slot_list[slot_id - 1]
-
-        slot = Slot(timeTable.dates[weekDayInt], start_time, end_time, timeTable, self)
->>>>>>> 3f846bb48fd90dd31dc6a15cb50b8e0a8c32fb9f
         self.slots.append(slot)
-        timeTable.events.append(slot)
+        self.timeTable.events.append(slot)
 
-<<<<<<< HEAD
     def addSlots(self, dictSlots:Dict[Days, List[int]]) :
         """
         Add the slots at which this class is being conducted
@@ -69,9 +51,6 @@ class Course :
             course = Course('🧪 Engineering Chemistry : Theory', timeTable)
             courses.addSlots( { Days.Monday : [1, 3], Days.Friday : [3] } )
         """
-=======
-    def addSlots(self, timeTable: TimeTable, dictSlots:Dict[str, List[int]]) :
->>>>>>> 3f846bb48fd90dd31dc6a15cb50b8e0a8c32fb9f
         for day, slots in dictSlots.items():
             for slot in slots :
                 self.addSlot(timeTable, day, slot)
